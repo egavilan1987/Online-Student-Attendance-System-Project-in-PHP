34135a61bdbdb7cc38434ecea3d913528b4f3ed2
egavilan1987 <enmanuelgc@gmail.com>
2019-05-23 16:21:40 -0400

    Create database_connection.php

---
+++ admin/database_connection.php
@@ -0,0 +1,9 @@
+<?php
+
+//database_connection.php
+
+$connect = new PDO("mysql:host=localhost;dbname=attendance","root","");
+
+$base_url = "http://localhost/tutorial/student-attendance-system-in-php-using-ajax/";
+
+?>
