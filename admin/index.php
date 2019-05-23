49ebc7dfb2c87144091857e957a792c2755dfac0
egavilan1987 <enmanuelgc@gmail.com>
2019-05-23 16:23:19 -0400

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<?php
+
+//index.php
+
+include('header.php');
+
+?>
+
+<div class="container" style="margin-top:30px">
+  
+</div>
+
+</body>
+</html>
+
+<script>
+$(document).ready(function(){
+ 
+
+});
+</script>
